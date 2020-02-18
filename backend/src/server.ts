51618ac5aaaedcf7de8e8d2f22b289374c914425
jonathanwj <jonathanwj@users.noreply.github.com>
2020-02-18 16:52:58 +0800

    Add server rate limiting for API

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,6 +5,8 @@ import cors from "cors";
 const app = express();
 const port = 3001;
 
+let nextAllowedQueryTime: number = 0;
+
 app.get("/", async (req, res) => {
   res.send("Hello Wor;d!");
 });
@@ -15,19 +17,45 @@ app.get("/search/:searchType", cors(), async (req, res) => {
   let sort: string = req.query.sort;
   let order: string = req.query.order;
 
+  if (currentTimeInSeconds() < nextAllowedQueryTime) {
+    res.sendStatus(429);
+    return;
+  }
+
   try {
     let githubRes = await queryGithubApi(searchType, query, sort, order);
-    // console.log("success:", githubRes);
+    // console.log(Math.round(Date.now() / 1000));
+    // console.log("success1:", githubRes.headers["x-ratelimit-limit"]);
+    // console.log("success2:", githubRes.headers["x-ratelimit-remaining"]);
+    // console.log("succes3:", githubRes.headers["x-ratelimit-reset"]);
+
+    let rateLimitRemaining: number = githubRes.headers["x-ratelimit-remaining"];
+    let rateLimitResetTime: number = githubRes.headers["x-ratelimit-reset"];
+    let currTime: number = currentTimeInSeconds();
+
+    if (currTime >= rateLimitResetTime) {
+      nextAllowedQueryTime = currTime;
+    } else {
+      let secondsToNextAllowedQuery = Math.round(
+        (rateLimitResetTime - currTime) / rateLimitRemaining
+      );
+      nextAllowedQueryTime = currTime + secondsToNextAllowedQuery;
+    }
+    // console.log("next allowed fimte", nextAllowedQueryTime);
     if (githubRes.status === 200) {
       res.json(githubRes.data);
     }
   } catch (error) {
-    // console.log("error:", error.response);
+    // console.log("error:", error.response.headers);
     res.json(error.response.data);
   }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+function currentTimeInSeconds() {
+  return Math.round(Date.now() / 1000);
+}
 
 async function queryGithubApi(
   searchType: string,
