a2b273895a564c02ffb11d5e5e9745ae3c90e9e0
jonathanwj <jonathanwj@users.noreply.github.com>
2020-02-18 11:25:58 +0800

    Initial project setup

---
+++ backend/src/server.ts
@@ -0,0 +1,7 @@
+import express from "express";
+const app = express();
+const port = 3001;
+
+app.get("/", (req, res) => res.send("Hello Word!"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
