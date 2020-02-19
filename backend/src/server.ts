a94f3a6b8714310648bef9abfb1259dfe43f710e
jonathanwj <jonathanwj@users.noreply.github.com>
2020-02-19 19:37:37 +0800

    Removed unused comments from server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,66 +7,66 @@ const port = 3001;
 
 let nextAllowedQueryTime: number = 0;
 
-app.get("/", async (req, res) => {
-  res.send("Hello Wor;d!");
-});
-
+const TOO_MANY_REQUESTS_STATUS = 429;
+// github search endpoint route
 app.get("/search/:searchType", cors(), async (req, res) => {
   let searchType: string = req.params.searchType;
   let query: string = req.query.q;
   let sort: string = req.query.sort;
   let order: string = req.query.order;
 
+  // limit number of requests send to github API
   if (currentTimeInSeconds() < nextAllowedQueryTime) {
-    res.sendStatus(429);
+    res.sendStatus(TOO_MANY_REQUESTS_STATUS);
     return;
   }
 
   try {
     let githubRes = await queryGithubApi(searchType, query, sort, order);
-    // console.log(Math.round(Date.now() / 1000));
-    // console.log("success1:", githubRes.headers["x-ratelimit-limit"]);
-    // console.log("success2:", githubRes.headers["x-ratelimit-remaining"]);
-    // console.log("succes3:", githubRes.headers["x-ratelimit-reset"]);
 
     let rateLimitRemaining: number = githubRes.headers["x-ratelimit-remaining"];
     let rateLimitResetTime: number = githubRes.headers["x-ratelimit-reset"];
     let currTime: number = currentTimeInSeconds();
 
     if (currTime >= rateLimitResetTime) {
+      // reset limit has passed
       nextAllowedQueryTime = currTime;
     } else {
+      // divide remaining time left by number of rateLimitRemaining
+      // this prevents lockout by github API
       let secondsToNextAllowedQuery = Math.round(
         (rateLimitResetTime - currTime) / rateLimitRemaining
       );
       nextAllowedQueryTime = currTime + secondsToNextAllowedQuery;
     }
-    // console.log("next allowed fimte", nextAllowedQueryTime);
     if (githubRes.status === 200) {
+      // send exact data to requestor
       res.json(githubRes.data);
     }
   } catch (error) {
-    // console.log("error:", error.response.headers);
     res.json(error.response.data);
   }
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-function currentTimeInSeconds() {
-  return Math.round(Date.now() / 1000);
-}
-
+const GITHUB_API_URL = "https://api.github.com/search/";
 async function queryGithubApi(
   searchType: string,
   query: string,
   sort?: string,
   order?: string
 ) {
-  let fullQuery: string = "https://api.github.com/search/";
+  let fullQuery: string = GITHUB_API_URL;
   if (searchType) fullQuery = fullQuery.concat(searchType);
   if (query) fullQuery = fullQuery.concat("?q=" + query);
   if (sort) fullQuery = fullQuery.concat("&sort=" + sort);
   if (order) fullQuery = fullQuery.concat("&order=" + order);
   return await axios.get(fullQuery);
 }
+
+function currentTimeInSeconds() {
+  return Math.round(Date.now() / 1000);
+}
+
+app.listen(port, () =>
+  console.log(`AutocompleteService API listening on port ${port}!`)
+);
